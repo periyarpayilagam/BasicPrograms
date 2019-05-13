f0ce956b9089b03850ed94b311d5c1b8a2f336e3
Periyar Payilagam <49941426+periyarpayilagam@users.noreply.github.com>
2019-05-13 19:13:09 +0530

    Create RangeOfPrime.java

---
+++ RangeOfPrime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class RangeOfPrime
+{
+    public static void main(String[] args)
+   {
+	 
+	 for(int i=50; i<=100; i++)//50,51, 52...100
+	 {
+		    boolean check=false;
+			 for(int start=2; start<i/2; start++)
+			 {
+				 if(i%start==0)
+				 {
+					 check=true;
+					 break;//To reduce the comparison
+				 }
+			 }
+			 if(check==false)
+			 {
+				 System.out.println(i);
+			 }
+	  }
+	 
+	 
+   }    
+}
